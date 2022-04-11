5bf801842f1b3f3203042a7ad9fd776e25a06c4d
yeto <yeto@jops.co.jp>
2022-04-11 21:02:58 +0900

    🔧 task, lock 修正

--- cli.ts
+++ cli.ts
@@ -1,4 +1,30 @@
 import { jopsColorAA } from "~/lib/asciiArt.ts";
-import { think } from "~/lib/balloon.ts";
+import { say, think } from "~/lib/balloon.ts";
+import { Command, readLines } from "~/deps.ts";
 
-console.log(think("こんにちわ\n赤ちゃん\nまたの名をよっちゃん") + jopsColorAA);
+const VERSION = "0.1.0";
+
+async function main() {
+  const stdin = [];
+  const isatty = Deno.isatty(Deno.stdin.rid);
+  if (!isatty) {
+    for await (const line of readLines(Deno.stdin)) {
+      stdin.push(line);
+    }
+  }
+
+  const { args, options } = await new Command()
+    .name("jopsay")
+    .version(VERSION)
+    .description("")
+    .option("-t, --think", "")
+    .arguments("<text:string>")
+    .parse(isatty ? Deno.args : [...Deno.args, stdin.join("\n")]);
+
+  const format = options.think ? think : say;
+  console.log(format(args[0]) + jopsColorAA);
+}
+
+if (import.meta.main) {
+  await main();
+}
--- deps.ts
+++ deps.ts
@@ -1,1 +1,3 @@
 export { default as stringWidth } from "https://cdn.skypack.dev/string-width";
+export { Command } from "https://deno.land/x/cliffy@v0.23.0/command/mod.ts";
+export { readLines } from "https://deno.land/std@0.134.0/io/mod.ts";
